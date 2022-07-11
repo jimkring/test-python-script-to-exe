da2be0757bee5e6c95089aa7366359ddaf6983ff
Jim Kring <jim.kring@jki.net>
2022-07-10 21:05:03 -0700

    added pydantic dependency

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello word!")
+from pydantic import BaseModel
+
+class Message(BaseModel):
+    value: str
+
+message_json = {'value': 'hello word!'}
+
+message = Message(message_json)
+
+print(f'pydantic says, "message.value"')
